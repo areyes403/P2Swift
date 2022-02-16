7baacc80f56da1c852458410ec9cfce908cae0e8
mac17 <mac17@iMac-de-mac17.local>
2022-02-16 09:14:41 -0600

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica 2
+//
+//  Created by mac17 on 16/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
